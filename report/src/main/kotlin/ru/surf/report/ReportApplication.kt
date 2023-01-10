d159414169a99b4da5f869e47c15a77d0800607c
Ivanesca <elotomp@gmail.com>
2023-01-10 23:11:01 +0300

    TASK-8 Add empty Report module

---
+++ report/src/main/kotlin/ru/surf/report/ReportApplication.kt
@@ -0,0 +1,11 @@
+package ru.surf.report
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReportApplication>(*args)
+}
---
+++ report/src/test/kotlin/ru/surf/report/ReportApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.surf.report
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReportApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
