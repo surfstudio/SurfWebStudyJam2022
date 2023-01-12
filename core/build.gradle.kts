fba9b325ad873f7e1cf8f87798d6bbbefe399d16
Ivanesca <elotomp@gmail.com>
2023-01-12 14:25:41 +0300

    TASK-32 Add initial migration script and test docker db

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -20,7 +20,13 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 }
 
 tasks.withType<KotlinCompile> {
