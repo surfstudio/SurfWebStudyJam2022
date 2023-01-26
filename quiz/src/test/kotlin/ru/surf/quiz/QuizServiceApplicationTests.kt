f1556a719805e55229169a7a0f25926e2a9df707
uncmmath <uncmmath@gmail.com>
2023-01-26 17:16:36 +0300

    Add empty quiz module

---
+++ quiz/src/test/kotlin/ru/surf/quiz/QuizServiceApplicationTests.kt
@@ -0,0 +1,14 @@
+package ru.surf.quiz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+class QuizServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
